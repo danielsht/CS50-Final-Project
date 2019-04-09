b87b330881c5155776f5adcf1e58094ff4016403
Daniel Shteremberg <dshteremberg@smu.edu>
2019-04-09 11:27:25 -0700

    refactored upcoming events to a new component file

---
+++ components/UpcomingEventRow.js
@@ -0,0 +1,46 @@
+import React from 'react'
+import { Text, View, StyleSheet } from 'react-native'
+
+getDateOfEvents = (eventISO) => {
+    const date = new Date(eventISO)
+    let options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' }
+    const dateFormatted = new Intl.DateTimeFormat('en-US', options).format(date)
+    return dateFormatted
+}
+
+getTimeOfEvent = (dateISO) => {
+    const date = new Date(dateISO)
+    const options = { hour12: true, hour: 'numeric', minute: '2-digit' }
+    const dateFormatted = new Intl.DateTimeFormat('en-US', options).format(date)
+    return dateFormatted
+}
+
+getEventTitles = (obj, index) => {
+    return (
+        <View key={index}>
+            <Text style={styles.EventTitle}>{obj.EventTitle}</Text>
+            <Text style={styles.EventDetails}>{this.getDateOfEvents(obj.When.Start)}</Text>
+            <Text style={styles.EventDetails}>{this.getTimeOfEvent(obj.When.Start)}</Text>
+            <Text style={styles.EventDetails}>{this.getTimeOfEvent(obj.When.End)}</Text>
+        </View>
+    )
+}
+
+export const UpcomingEvents = ( props ) => (
+    <View>
+        {props.events.map((val, index) => this.getEventTitles(val, index))}
+    </View>
+)
+
+const styles = StyleSheet.create({
+    EventTitle: {
+        fontFamily: 'robotoMed',
+        fontSize: 20,
+        color: '#BF5B20',
+    },
+    EventDetails: {
+        fontFamily: 'robotoReg',
+        fontSize: 12,
+        color: '#BF5B20',
+    }
+})
