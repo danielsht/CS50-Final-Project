1a1157aed1c0ce797782c6a4e81c4bed77538a3c
Daniel Shteremberg <dshteremberg@smu.edu>
2019-04-24 11:41:28 -0700

    Added Image Support to Events page, Hard coded for now, will be passed as prop later

--- components/UpcomingEventRow.js
+++ components/UpcomingEventRow.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Text, View, StyleSheet } from 'react-native'
+import { Text, View, StyleSheet, Image } from 'react-native'
 
 getDateOfEvent = (eventISO) => {
     const options = { weekday: 'short', year: 'numeric', month: 'long', day: 'numeric' }
@@ -13,22 +13,44 @@ getTimeOfEvent = (dateISO) => {
 
 getEventTitles = (obj, index) => {
     return (
-        <View key={index}>
-            <Text style={styles.EventTitle}>{obj.EventTitle}</Text>
-            <Text style={styles.EventDetails}>{this.getDateOfEvent(obj.When.Start)}</Text>
-            <Text style={styles.EventDetails}>{this.getTimeOfEvent(obj.When.Start)}</Text>
-            <Text style={styles.EventDetails}>{this.getTimeOfEvent(obj.When.End)}</Text>
+        <View key={index} style={styles.Container}>
+            <View style={styles.EventImageContainer}>
+                <Image source={require('../Icons/logo.png')} style={styles.EventImage}/>
+            </View>
+            <View style={styles.EventDetContainer}>
+                <Text style={styles.EventTitle}>{obj.EventTitle}</Text>
+                <Text style={styles.EventDetails}>{this.getDateOfEvent(obj.When.Start)}</Text>
+                <Text style={styles.EventDetails}>{this.getTimeOfEvent(obj.When.Start)}</Text>
+                <Text style={styles.EventDetails}>{this.getTimeOfEvent(obj.When.End)}</Text>
+            </View>
         </View>
     )
 }
 
-export const UpcomingEvents = ( props ) => (
+export const UpcomingEvents = (props) => (
     <View>
         {props.events.map((val, index) => this.getEventTitles(val, index))}
     </View>
 )
 
 const styles = StyleSheet.create({
+    Container: {
+        flexDirection: 'row',
+        paddingTop: 20,
+    },
+    EventImageContainer: {
+        flex: 1,
+    }, 
+    EventImage: {
+        resizeMode: 'contain', 
+        flex: 1, 
+        width: undefined, 
+        height: undefined,
+    },
+    EventDetContainer: {
+        flex: 1,
+        flexDirection: 'column',
+    },
     EventTitle: {
         fontFamily: 'robotoMed',
         fontSize: 20,
@@ -38,5 +60,5 @@ const styles = StyleSheet.create({
         fontFamily: 'robotoReg',
         fontSize: 12,
         color: '#BF5B20',
-    }
+    }, 
 })
